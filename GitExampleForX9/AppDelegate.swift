d9e86a16b8bf546975e01b6428dc68c47ab2daa5
Thien Le <thien.lequang@asnet.com.vn>
2018-03-03 10:25:05 +0700

    Initial Commit

---
+++ GitExampleForX9/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitExampleForX9
+//
+//  Created by Thien Le quang on 3/3/18.
+//  Copyright © 2018 Thien Le quang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ GitExampleForX9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitExampleForX9
+//
+//  Created by Thien Le quang on 3/3/18.
+//  Copyright © 2018 Thien Le quang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
