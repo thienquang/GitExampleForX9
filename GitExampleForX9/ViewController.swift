c5573df5d59c06421ed05b98ab58bba1c307952b
Thien Le <thien.lequang@asnet.com.vn>
2018-03-03 10:36:30 +0700

    to master

--- GitExampleForX9/ViewController.swift
+++ GitExampleForX9/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: NSViewController {
     super.viewDidLoad()
 
     // Do any additional setup after loading the view.
-    print("Some change in example-branch")
+    print("Some change in example-branch - now in master")
   }
 
   override var representedObject: Any? {
